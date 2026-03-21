4b7b6e517f8f55835e6920dba518c564f6bbae31
Raposobl <leonardoraposoboechat@gmail.com>
2026-03-21 11:45:47 -0300

    finalizado?

---
+++ lib/site-config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  nomeLoja: "Helena Noivas",
+  nomeCurto: "HELENA",
+  telefone: "(22) 99999-9999",
+  whatsappMsg: "Olá! Gostaria de saber mais sobre o vestido...",
+  endereco: "Rua Principal, 123 - Centro, Araruama - RJ",
+  instagram: "@helenanoivas",
+  corPrimaria: "#db2777", // Rosa pink (para mudares no tailwind depois)
+}
