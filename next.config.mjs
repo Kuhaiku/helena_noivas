a10e88619749819b4a858618e30fb19da71f3622
Raposobl <leonardoraposoboechat@gmail.com>
2026-03-24 15:41:15 -0300

    arrumando os .enaav

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  compress: true, // Adicione isto para ajudar no serviço de arquivos
+  output: "standalone", 
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true, // Adicione isto para evitar travas de lint no deploy
-  },
   images: {
-    unoptimized: true, // Tente ativar isso temporariamente para resolver o erro 400 das imagens
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "anjodanoiva.com.br" },
